58317a0260f114c3b5c5daeeac88035531ffabe1
Cross, Julian M <jcross@blm.gov>
2026-03-09 13:57:53 -0600

    Bug fix on last commit

--- hipp/dataquery/dataquery.py
+++ hipp/dataquery/dataquery.py
@@ -275,9 +275,8 @@ def EE_download_images_to_disk(
             # JMC edit: EE is inconsistent with when files are zipped, there can be a mix
             print(len(images), 'images downloaded.')
             # Trigger unzip if any .gz file exists in the output directory
-            if any(out.glob('*.gz')):
-                hipp.io.gzip_dir(out)
-
+            if any(images.glob('*.gz')):
+                hipp.io.gzip_dir(images)
 
             hipp.io.move_files(output_directory, images_directory, '.tif')
             hipp.io.move_files(output_directory, calibration_reports_directory, '.pdf')
